67a53f6b2d5c593558441b87b4ca2825ab9160c7
irotsoma <irotsomadev@gmail.com>
2016-11-02 16:47:28 -0700

    Trying to fix integration tests on travis

--- centralcontroller/src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/Startup.kt
+++ centralcontroller/src/main/kotlin/com/irotsoma/cloudbackenc/centralcontroller/Startup.kt
@@ -22,10 +22,10 @@ package com.irotsoma.cloudbackenc.centralcontroller
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.system.ApplicationPidFileWriter
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 open class CentralController
 
 fun main(args: Array<String>) {
